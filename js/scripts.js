7d7490aaf91a5551ed66a472025c9add09093ba8
James Wyn <thisisjameswyn@gmail.com>
2021-01-15 09:44:09 -0800

    add more branching logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   $("form#welcomeSelect").submit(function(event) {
     const operator = $("input:radio[name=operator]:checked").val();
-    const age = parseInt($("input#age").val());
-    const gender = $("select#gender").val();
-
-    const quote = (100 - age) * 3;
-
-    $("#rate").text(quote);
-    $("#quote").show();
+    
+    if(operator === "yes") {
+      $(".question1").show();
+      $(".question2").hide();
+    } else {
+      $(".question2").show();
+      $(".question1").hide();
+    }
 
     event.preventDefault();
   });
