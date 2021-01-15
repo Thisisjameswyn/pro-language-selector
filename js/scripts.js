bb19c06e5cc0de6b77d645789e7003e1646c2f4b
James Wyn <thisisjameswyn@gmail.com>
2021-01-15 13:51:49 -0800

    finalize all branching logic, css cleanup, tweak site apperance

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,25 @@
 $(document).ready(function() {
   $("form#welcomeSelect").submit(function(event) {
+    const userName = $("input#name").val();
     const op1 = parseInt($("input:radio[name=question1]:checked").val());
     const op2 = parseInt($("input:radio[name=question2]:checked").val());
     const op3 = parseInt($("input:radio[name=question3]:checked").val());
     const op4 = parseInt($("input:radio[name=question4]:checked").val());
-    const total = op1+op2+op3+op4;
+    const op5 = parseInt($("input:radio[name=question5]:checked").val());
+    const total = op1+op2+op3+op4+op5;
+    console.log(userName);
 
-    if(total >= 7) {
+    $("#name").text(userName);
+
+    if(total >= 9) {
       $(".welcome").hide();
       $(".questions").hide();
       $("#answer1").toggle("slow");
-    }else if(total < 7 && total > 4) {
+    }else if(total <= 8 && total > 6) {
       $(".welcome").hide();
       $(".questions").hide();
       $("#answer2").toggle("slow");
-    }else if(total === 4){
+    }else if(total < 6){
       $(".welcome").hide();
       $(".questions").hide();
       $("#answer3").toggle("slow");
